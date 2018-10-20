185d960c0d439b9d09dbe2816abe9d87626a43db
aluno <aluno@LABMAC09.local>
2018-10-20 15:23:53 -0300

    started

---
+++ ExemploDataBinding/ExemploDataBinding/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ExemploDataBinding
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Completed(object sender, System.EventArgs e)
+        {
+            areaBrowser.Source = url.Text;
+
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            areaBrowser.GoBack();
+        }
+
+        void Handle_Clicked_1(object sender, System.EventArgs e)
+        {
+            areaBrowser.GoForward();
+        }
+    }
+}
